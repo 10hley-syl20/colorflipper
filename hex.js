032b6d2b41824b22b42fe5a99d01218389e3342f
10hley-syl20 <64177680+10hley-syl20@users.noreply.github.com>
2020-07-23 08:24:52 -0400

    Create hex.js

---
+++ hex.js
@@ -0,0 +1,33 @@
+
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+
+let colors = ["green", "red", "rgba(133,122,200)", "#f15025"]; 
+let btn = document.querySelector("#btn");
+let color = document.querySelector(".color");
+
+
+
+
+
+
+
+
+
+
+btn.addEventListener('click',function(){
+   
+    let hexColor = '#';
+    for(let i=0; i < 6; i++){
+        hexColor += hex[getrandomHex()];
+    }
+    
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor
+});
+
+function getrandomHex(){
+   return Math.floor(Math.random() * hex.length);
+    
+}
